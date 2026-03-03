213955b1171b1161abc63b71a76af40bc8f7f9ea
foksiny <icarojose533@gmail.com>
2026-03-03 19:26:16 -0300

    Better syntax highlighting

--- syntax_highlight/c5-mode.el
+++ syntax_highlight/c5-mode.el
@@ -1,133 +1,76 @@
-;;; c5-mode.el --- Major mode for editing C5 programming language files
+;;; c5-mode.el --- Major mode for the C5 programming language -*- lexical-binding: t -*-
 
-;; Author: Jose
-;; Version: 1.0
-;; Keywords: languages c5
+;; Copyright (C) 2026 Gemini CLI
+;; Author: Gemini CLI
+;; Keywords: languages
+
 ;;; Commentary:
-
-;; Major mode for editing C5 (C5) source code.
+;; This mode provides syntax highlighting and basic indentation for the C5
+;; programming language.
 
 ;;; Code:
 
-(defgroup c5 nil
-  "Major mode for editing C5 code."
-  :group 'languages)
-
-(defcustom c5-mode-hook nil
-  "Hook run when entering C5 mode."
-  :type 'hook
-  :group 'c5)
-
-;; Syntax table
 (defvar c5-mode-syntax-table
-  (let ((table (make-syntax-table)))
-    ;; C-style comments
-    (modify-syntax-entry ?/ ". 124" table)
-    (modify-syntax-entry ?* ". 23b" table)
-    (modify-syntax-entry ?\n "> b" table)
-    ;; String and character literals
-    (modify-syntax-entry ?\" "\"" table)
-    (modify-syntax-entry ?\' "\"" table)
-    ;; Word constituents
-    (modify-syntax-entry ?_ "w" table)
-    ;; Parentheses
-    (modify-syntax-entry ?\( "()" table)
-    (modify-syntax-entry ?\) ")(" table)
-    (modify-syntax-entry ?\{ "(}" table)
-    (modify-syntax-entry ?\} "){" table)
-    (modify-syntax-entry ?\[ "(]" table)
-    (modify-syntax-entry ?\] ")[" table)
-    ;; Operators
-    (modify-syntax-entry ?= "." table)
-    (modify-syntax-entry ?< "." table)
-    (modify-syntax-entry ?> "." table)
-    (modify-syntax-entry ?+ "." table)
-    (modify-syntax-entry ?- "." table)
-    (modify-syntax-entry ?* "." table)
-    (modify-syntax-entry ?/ "." table)
-    (modify-syntax-entry ?% "." table)
-    (modify-syntax-entry ?& "." table)
-    (modify-syntax-entry ?| "." table)
-    (modify-syntax-entry ?^ "." table)
-    (modify-syntax-entry ?~ "." table)
-    (modify-syntax-entry ?! "." table)
-    table)
+  (let ((st (make-syntax-table)))
+    ;; C-style comments: // and /* */
+    (modify-syntax-entry ?/ ". 124b" st)
+    (modify-syntax-entry ?* ". 23" st)
+    (modify-syntax-entry ?\n "> b" st)
+    ;; Strings
+    (modify-syntax-entry ?\" "\"" st)
+    (modify-syntax-entry ?\\ "\\" st)
+    ;; Characters
+    (modify-syntax-entry ?\' "\"" st)
+    st)
   "Syntax table for `c5-mode'.")
 
-;; Keywords
-(defconst c5-keywords
-  '("include" "void" "signed" "unsigned" "const" "let" "if" "else" "while"
-    "do" "for" "foreach" "in" "switch" "case" "default" "break" "struct"
-    "enum" "macro" "fnct" "type" "return" "try" "catch"))
+(defvar c5-keywords
+  '("include" "libinclude" "let" "const" "macro" "type" "fnct"
+    "if" "else" "switch" "case" "default"
+    "while" "do" "for" "foreach" "in" "break"
+    "return" "try" "catch" "#static" "#dynamic")
+  "Keywords for C5.")
 
-;; Type keywords (parameterized types like int<32>)
-(defconst c5-type-keywords
-  '("int<8>" "int<16>" "int<32>" "int<64>" "float<32>" "float<64>" "char" "string"))
+(defvar c5-types
+  '("void" "int" "float" "char" "string" "array" "signed" "unsigned")
+  "Built-in types for C5.")
 
-;; Ellipsis (...)
-(defconst c5-ellipsis
-  '("\\.\\.\\."))
-
-;; Regular expressions
-(defconst c5-font-lock-keywords
-  `(
-    ;; Comments
-    ("/\\*\\([^/*]*\\|\\*[^/]\\)*\\*/" . font-lock-comment-face)
-    ("//.*" . font-lock-comment-face)
-
-    ;; Strings and characters
-    ("\"[^\"\\]*\\(\\\\.[^\"\\]*\\)*\"" . font-lock-string-face)
-    ("'[^'\\]*'" . font-lock-string-face)
-
-    ;; Keywords
-    (,(regexp-opt c5-keywords 'words) . font-lock-keyword-face)
-;; Types (int<32>, float<64>, etc.)
-(,(regexp-opt '("int" "float" "char" "string") 'words) . font-lock-type-face)
-
-;; Struct/Enum/Type definition names
-("\\<\\(struct\\|enum\\|type\\)\\>\\s-+\\([a-zA-Z_][a-zA-Z0-9_]*\\)"
- 2 font-lock-type-face)
-
-;; Ellipsis (...)
-(,(regexp-opt c5-ellipsis) . font-lock-builtin-face)
-
-    ;; Numbers
-    ("\\b[0-9]+\\b" . font-lock-constant-face)
-    ("\\b0x[0-9a-fA-F]+\\b" . font-lock-constant-face)
-    ("\\b[0-9]+\\.[0-9]*\\([eE][-+]?[0-9]+\\)?\\b" . font-lock-constant-face)
-
-    ;; Preprocessor-like include directive
-    ("^\\s-*include\\s-+" . font-lock-preprocessor-face)
-
-    ;; Namespace resolution (::)
-    ("::" . font-lock-builtin-face)
-
-    ;; Labels (for case statements and goto)
-    ("\\b[a-zA-Z_][a-zA-Z0-9_]*\\s-*:" . font-lock-label-face)
-
-    ;; Struct/Enum member access (->)
-    ("->" . font-lock-function-call-face)
-
-    ;; Function calls
-    ("\\b[a-zA-Z_][a-zA-Z0-9_]*\\s-*(" . (1 font-lock-function-call-face))
-
-    ;; Variables (simple heuristic)
-    ("\\b[a-zA-Z_][a-zA-Z0-9_]*\\b" . font-lock-variable-name-face)
-    ))
+(defvar c5-font-lock-keywords
+  (list
+   ;; Keywords
+   (cons (regexp-opt c5-keywords 'words) font-lock-keyword-face)
+   ;; Built-in types
+   (cons (regexp-opt c5-types 'words) font-lock-type-face)
+   ;; Parameterized types like int<32> or float<64>
+   '("\\<\\(int\\|float\\)<[0-9]+>\\>" . font-lock-type-face)
+   ;; Structs and Enums
+   '("\\<\\(struct\\|enum\\)\\>\\s-+\\([a-zA-Z_][a-zA-Z0-9_]*\\)"
+     (1 font-lock-keyword-face)
+     (2 font-lock-variable-name-face))
+   ;; Function definitions
+   '("\\<\\([a-zA-Z_][a-zA-Z0-9_]*\\)\\s-*("
+     (1 font-lock-function-name-face))
+   ;; Namespace resolution
+   '("\\<\\([a-zA-Z_][a-zA-Z0-9_]*\\)::"
+     (1 font-lock-constant-face))
+   ;; Constants (integers and floats)
+   '("\\<[0-9]+\\(?:\\.[0-9]+\\)?\\>" . font-lock-constant-face)
+   '("\\<0x[0-9a-fA-F]+\\>" . font-lock-constant-face))
+  "Font-lock keywords for `c5-mode'.")
 
 ;;;###autoload
 (define-derived-mode c5-mode prog-mode "C5"
-  "Major mode for editing C5 code."
+  "Major mode for editing C5 files."
   :syntax-table c5-mode-syntax-table
   (setq-local font-lock-defaults '(c5-font-lock-keywords))
   (setq-local comment-start "// ")
-  (setq-local comment-start-skip "//+\\s-*")
   (setq-local comment-end "")
-  (setq-local indent-tabs-mode nil)
-  (setq-local c-basic-offset 4))
+  ;; Basic indentation based on brackets
+  (setq-local indent-line-function 'indent-relative))
 
 ;;;###autoload
 (add-to-list 'auto-mode-alist '("\\.c5\\'" . c5-mode))
+;;;###autoload
 (add-to-list 'auto-mode-alist '("\\.c5h\\'" . c5-mode))
 
 (provide 'c5-mode)
--- syntax_highlight/c5.vim
+++ syntax_highlight/c5.vim
@@ -1,100 +1,68 @@
 " Vim syntax file
 " Language: C5
-" Maintainer: Jose
-" URL: https://github.com/yourusername/c5
+" Maintainer: Gemini CLI
+" Latest Revision: 2026-03-03
 
 if exists("b:current_syntax")
   finish
 endif
 
 " Keywords
-syn keyword c5Keyword include void signed unsigned const let if else while do for foreach in switch case default break struct enum macro fnct type return try catch
-syn keyword c5Type int float char string
+syn keyword c5Keyword include libinclude let const macro type fnct
+syn keyword c5Keyword #static #dynamic
+syn keyword c5Conditional if else switch case default
+syn keyword c5Repeat while do for foreach in break
+syn keyword c5Statement return try catch
+syn keyword c5Structure struct enum
+syn keyword c5Type void int float char string array signed unsigned
 
+" Parameterized Types (e.g., int<32>, float<64>)
+syn match c5ParameterizedType "\v(int|float)\<\d+\>"
 
-" Include directive
-syn match c5Include '^\s*include\s*[<"]' contains=c5IncludePunct
-syn match c5IncludePunct '[<"]' contained
+" Namespaces and Operators
+syn match c5Namespace "\v[a-zA-Z_][a-zA-Z0-9_]*::"
+syn match c5Operator "\v-\>"
+syn match c5Operator "\v\."
+syn match c5Operator "\v\+\=|\-\=|\*\=|\/\=|\%\=|\&\=|\"|\|\=|\^\=|\<\<\=|\>\>\="
+syn match c5Operator "\v\+\+|\-\-"
+syn match c5Operator "\v\=\="
+syn match c5Operator "\v\!\="
+syn match c5Operator "\v\<\="
+syn match c5Operator "\v\>\="
+syn match c5Operator "\v\&\&"
+syn match c5Operator "\v\|\|"
+syn match c5Operator "\v[\+\-\*\/\%\=\!\<\>\&\|\^\~\?]"
 
 " Numbers
-syn match c5Number '\b[0-9]\+\(_\?[0-9]\+\)*\b'
-syn match c5Number '\b0x[0-9a-fA-F]\+\(_\?[0-9a-fA-F]\+\)*\b'
-syn match c5Number '\b[0-9]\+\.[0-9]*\([eE][-+]?[0-9]\+\)\?\b'
+syn match c5Number "\v<\d+>"
+syn match c5Float "\v<\d+\.\d+>"
+syn match c5Hex "\v<0x[0-9a-fA-F]+>"
 
-" Strings
-syn region c5String start=+"+ skip=+\\\\\|\\"+ end=+"+ contains=c5Escape
-syn region c5String start=+'+ skip=+\\\\\|\\'+ end=+'+ contains=c5Escape
-syn match c5Escape '\\[abfnrtv\\"']' contained
-syn match c5Escape '\\x\x\{2}' contained
-syn match c5Escape '\\u\x\{4}' contained
-syn match c5Escape '\\U\x\{8}' contained
+" Strings and Characters
+syn region c5String start=/\v"/ skip=/\v\\./ end=/\v"/ contains=c5Escape
+syn region c5Character start=/\v'/ skip=/\v\\./ end=/\v'/ contains=c5Escape
+syn match c5Escape "\v\\." contained
 
 " Comments
-syn region c5Comment start='/\*' end='\*/' contains=c5Todo
-syn region c5Comment start='//' end='$' contains=c5Todo
+syn match c5Comment "\v\/\/.*$"
+syn region c5Comment start=/\v\/\*/ end=/\v\*\//
 
-" Todo
-syn keyword c5Todo TODO FIXME XXX NOTE HACK contained
-
-" Labels (for case statements)
-syn match c5Label '^\s*[a-zA-Z_][a-zA-Z0-9_]*\s*:' contains=c5LabelPunct
-syn match c5LabelPunct ':' contained
-
-" Operators
-syn match c5Operator '[=!<>]=\|<<\|>>\|&&|||[&|^~+*/%-]'
-
-" Delimiters
-syn match c5Delimiter '[(){}[\],.;]'
-
-" Namespace resolution
-syn match c5Namespace '::'
-
-" Arrow operator
-syn match c5Arrow '->'
-
-" Type parameters (angle brackets)
-syn match c5TypeParam '<[^>]*>' contains=c5TypeParamPunct
-syn match c5TypeParamPunct '[<>]' contained
-
-" User-defined type names (struct, enum, type)
-syn match c5UserType "\<\(struct\|enum\|type\)\s\+\zs\k\+\ze"
-hi def link c5UserType Type
-
-" Ellipsis (...)
-syn match c5Ellipsis "\.\.\."
-hi def link c5Ellipsis Special
-
-" Function call
-syn match c5FunctionCall '\k\+\s*(' contains=c5FunctionCallPunct
-syn match c5FunctionCallPunct '(' contained
-
-" Macro definition
-syn match c5Macro '^\s*macro\s\+\k\+' contains=c5MacroName
-syn match c5MacroName '\k\+' contained
-
-" Highlighting links
+" Highlighting Links
 hi def link c5Keyword Keyword
+hi def link c5Conditional Conditional
+hi def link c5Repeat Repeat
+hi def link c5Statement Statement
+hi def link c5Structure Structure
 hi def link c5Type Type
-hi def link c5Include Include
-hi def link c5IncludePunct Delimiter
-hi def link c5Number Number
-hi def link c5String String
-hi def link c5Escape SpecialChar
-hi def link c5Comment Comment
-hi def link c5Todo Todo
-hi def link c5Label Label
-hi def link c5LabelPunct Delimiter
+hi def link c5ParameterizedType Type
+hi def link c5Namespace Identifier
 hi def link c5Operator Operator
-hi def link c5Delimiter Delimiter
-hi def link c5Namespace Special
-hi def link c5Arrow Special
-hi def link c5TypeParam Type
-hi def link c5TypeParamPunct Delimiter
-hi def link c5FunctionCall Function
-hi def link c5FunctionCallPunct Delimiter
-hi def link c5Macro PreProc
-hi def link c5MacroName Function
+hi def link c5Number Number
+hi def link c5Float Float
+hi def link c5Hex Number
+hi def link c5String String
+hi def link c5Character Character
+hi def link c5Escape Special
+hi def link c5Comment Comment
 
 let b:current_syntax = "c5"
-
-" vim: ft=vim
